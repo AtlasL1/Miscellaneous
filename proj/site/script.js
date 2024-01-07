87ef1c610ff9388ceb1be108465fd28f7a9e2023
Atlas <loxingle1@gmail.com>
2024-01-07 20:50:35 +0800

    Create script.js

---
+++ proj/site/script.js
@@ -0,0 +1,32 @@
+const githubherf = document.querySelector(".githubherf")
+const githubButton = document.querySelector(".githubButton")
+
+githubButton.addEventListener("click", () => {
+    githubherf.click()
+})
+
+const line1 = document.querySelector(".line1")
+const line2 = document.querySelector(".line2")
+const line3 = document.querySelector(".line3")
+const nav_menu = document.querySelector(".nav_menu")
+const nav__list = document.querySelector(".nav__list")
+const nav__buttons = document.querySelector(".nav__buttons")
+nav_menu.addEventListener("click", () => {
+    nav__buttons.classList.toggle("active")
+    nav__list.classList.toggle("active")
+    line1.classList.toggle("active")
+    line2.classList.toggle("active")
+    line3.classList.toggle("active")
+})
+
+const projectshref = document.querySelector(".projectshref")
+const projectsButton = document.querySelector(".ProjectButton")
+
+projectsButton.addEventListener("click", () => {
+    projectshref.click()
+})
+
+window.addEventListener("load", () => {
+    document.querySelector("main").style.display = "block"
+    document.querySelector(".load").style.display = "none"
+})
